e2c2ea09cbb1076571fb239e090b507ce300666a
Ralf Geschke <ralf@kuerbis.org>
2022-03-12 17:29:32 +0100

    re-committed

---
+++ cmd/cli/processdata.go
@@ -0,0 +1,180 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/geschke/golrackpi"
+	"github.com/spf13/cobra"
+
+	//"log"
+	//"os"
+	"sort"
+	"strings"
+)
+
+var authData golrackpi.AuthClient
+var csvOutput bool = false
+var delimiter string = ","
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&authData.Password, "password", "p", "", "Password")
+	rootCmd.PersistentFlags().StringVarP(&authData.Server, "server", "s", "", "Server (e.g. inverter IP address)")
+	rootCmd.PersistentFlags().StringVarP(&authData.Scheme, "scheme", "m", "", "Scheme (http or https, default http)")
+
+	processdataGetCmd.Flags().BoolVarP(&csvOutput, "csv", "c", false, "Set output to CSV format")
+	processdataGetCmd.Flags().StringVarP(&delimiter, "delimiter", "d", ",", "Set CSV delimiter (default \",\")")
+
+	rootCmd.AddCommand(processdataCmd)
+	processdataCmd.AddCommand(processdataListCmd)
+	processdataCmd.AddCommand(processdataGetCmd)
+
+}
+
+var processdataCmd = &cobra.Command{
+	Use: "processdata",
+
+	Short: "List processdata values",
+	//Long:  `Manage dynpower domain entries in database.`,
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		handleProcessdata()
+	},
+}
+
+var processdataListCmd = &cobra.Command{
+	Use: "list",
+
+	Short: "List all available processdata and modules",
+	//Long:  `List all domains in the dynpower database. If a DSN is submitted by the flag --dsn, this DSN will be used. If no DSN is provided, dynpower-cli tries to use the environment variables DBHOST, DBUSER, DBNAME and DBPASSWORD.`,
+
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		listProcessdata()
+	},
+}
+
+var processdataGetCmd = &cobra.Command{
+	Use: "get [moduleid] [processdataid(s)]",
+
+	Short: "Get processdata values",
+	//Long:  `List all domains in the dynpower database. If a DSN is submitted by the flag --dsn, this DSN will be used. If no DSN is provided, dynpower-cli tries to use the environment variables DBHOST, DBUSER, DBNAME and DBPASSWORD.`,
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command,
+		args []string) {
+		getProcessdata(args)
+	},
+}
+
+func listProcessdata() {
+	lib := golrackpi.NewWithParameter(golrackpi.AuthClient{
+		Scheme:   authData.Scheme,
+		Server:   authData.Server,
+		Password: authData.Password,
+	})
+
+	ok, err := lib.Login()
+
+	fmt.Println("Ok?", ok)
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+
+	pd := lib.GetProcessDataList()
+	//fmt.Println("returned: ", pd)
+
+	moduleNames := make([]string, 0, len(pd))
+	for mn := range pd {
+		moduleNames = append(moduleNames, mn)
+	}
+
+	// sort the slice by keys
+	sort.Strings(moduleNames)
+
+	for _, moduleId := range moduleNames {
+		fmt.Println("Module:", moduleId)
+		fmt.Println("ProcessDataIds:")
+		for _, processDataIds := range pd[moduleId].ProcessDataIds {
+			fmt.Println("\t", processDataIds)
+
+		}
+		fmt.Println()
+	}
+
+	//fmt.Printf("%-"+fmt.Sprintf("%d", maxStrlen)+"s%-21s%-21s\n", domainname, dtCreated, dtUpdated)
+
+}
+
+func getProcessdata(args []string) {
+
+	fmt.Println("in getProcessdata")
+	fmt.Println(args)
+	moduleidArg := args[0]
+	processdataidArg := args[1]
+	moduleIds := strings.Split(moduleidArg, ",")
+	processdataIds := strings.Split(processdataidArg, ",")
+	fmt.Println("moduleids:", moduleIds)
+	fmt.Println("processdataids:", processdataIds)
+
+	if len(moduleIds) > 1 {
+		fmt.Println("Please enter only one moduleid.")
+		return
+	}
+
+	v := golrackpi.ProcessData{ModuleId: moduleIds[0], ProcessDataIds: processdataIds}
+
+	lib := golrackpi.NewWithParameter(golrackpi.AuthClient{
+		Scheme:   authData.Scheme,
+		Server:   authData.Server,
+		Password: authData.Password,
+	})
+
+	ok, err := lib.Login()
+
+	fmt.Println("Ok?", ok)
+	if err != nil {
+		fmt.Println(err)
+		panic(err.Error())
+	}
+	pdv := lib.GetProcessDataValues(v)
+	fmt.Println("processDataValues:", pdv)
+
+	moduleNames := make([]string, 0, len(pdv))
+	for mn := range pdv {
+		moduleNames = append(moduleNames, mn)
+	}
+
+	// sort the slice by keys
+	sort.Strings(moduleNames)
+
+	if csvOutput {
+		for _, moduleId := range moduleNames {
+			fmt.Printf("Module%sProcessdata Id%sProcessdata Unit%sProcessdata Value\n", delimiter, delimiter, delimiter)
+
+			for _, processData := range pdv[moduleId].ProcessData {
+				fmt.Printf("%s%s%s%s%s%s%v\n", moduleId, delimiter, processData.Id, delimiter, processData.Unit, delimiter, processData.Value)
+
+			}
+
+		}
+	} else {
+
+		for _, moduleId := range moduleNames {
+			fmt.Println("Module:", moduleId)
+			fmt.Println("ProcessDataValues (Id\tUnit\tValue):")
+			for _, processData := range pdv[moduleId].ProcessData {
+				fmt.Println(processData.Id, "\t", processData.Unit, "\t", processData.Value)
+				// todo: add better formatting
+
+			}
+			fmt.Println()
+		}
+	}
+}
+
+/*
+* Handle processdata-related commands
+ */
+func handleProcessdata() {
+	fmt.Println("\nUnknown or missing command.\nRun golrackpi processdata --help to show available commands.")
+}
